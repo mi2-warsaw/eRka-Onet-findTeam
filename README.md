c9d67e54ad3b428767c40c59475d9cf1d7e4ebf3
Michal Stechly <michal.stechly@gmail.com>
2015-11-28 17:06:17 +0100

    knn classifier + readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-﻿# eRka-Onet-findTeam
+# eRka-Onet-findTeam
 
 Repozytorium zespołu `find()` w trakcie [maratonu analizy danych](https://www.facebook.com/eRkaKrakow/photos/a.808395282559436.1073741831.736393729759592/954607501271546/?type=3&theater) organizowanego przez [`entuzajstów R krakowska alternatywa`](https://www.facebook.com/eRkaKrakow/) 
 
@@ -10,5 +10,5 @@ Repozytorium zespołu `find()` w trakcie [maratonu analizy danych](https://www.f
 >
 > Marcin Kosiński m.p.kosinski@gmail.com
 >
-> Michał
+> Michał Stęchły michal.stechly@gmail.com
 >
--- R_codes/09_knnClassifier.R
+++ R_codes/09_knnClassifier.R
@@ -1,3 +1,4 @@
+#Checking the meaning of the interpunction
 library("tidyr")
 ESRLabels <- cbind(DTM_tagged_interpunction$Classes, DTM_tagged_interpunction$EndSignRatio)
 ESRLabels <- as.data.frame(ESRLabels)
@@ -13,25 +14,35 @@ ggplot(ESRLabels, aes(factor(Tabl),Ratio) ) +
 library("class")
 library("gmodels")
 
-trainingSet <-train_interpunction$EndSignRatio
-testSet <-test_interpunction$EndSignRatio
+boruta_full <- rbind(train_Boruta, test_Boruta)
 
-train_labels <- train_interpunction$Classes
-test_labels <- test_interpunction$Classes
+train_labels <- train_Boruta$Classes
+test_labels <- test_Boruta$Classes
 
-test_pred <- knn(trainingSet, testSet, cl = train_labels, k=5)
+test_pred <- knn(train_Boruta, test_Boruta, cl = train_labels, k=5)
 
 CrossTable(test_labels,test_pred)
 
-ir_z <- as.data.frame(scale(ir_r[,-5]))
+train_Boruta %>% select(-Classes) -> train_Boruta_scaled
 
-ir_train <- head(ir_z,nrow(ir_z)-A)
-ir_test <- tail(ir_z,A)
-# re-classify test cases
-ir_test_pred <- knn(ir_train, ir_test, ir_train_labels,k=9)
+train_Boruta_scaled <- scale(train_Boruta_scaled)
+attrs <- attributes(train_Boruta_scaled)
+scale_center <- attr(train_Boruta_scaled,"scaled:center")
+scale_scale <- attr(train_Boruta_scaled,"scaled:scale")
+train_Boruta_scaled <- as.data.frame(train_Boruta_scaled)
+
+test_Boruta %>% select(-Classes) -> test_Boruta_scaled
+test_Boruta_scaled <- scale(test_Boruta_scaled, scale = scale_scale, center = scale_center)
+
+
+test_pred_scaled <- knn(train_Boruta_scaled, test_Boruta_scaled, train_labels,k=5)
 
 # Create the cross tabulation of predicted vs. actual
-CrossTable(ir_test_labels,ir_test_pred)
+CrossTable(test_pred_scaled,test_labels)
 
 #Percentage of well clasified cases:
-sum(ir_test_labels==ir_test_pred)/A
+sum(test_labels==test_pred)/length(test_labels)
+sum(test_labels==test_pred_scaled)/length(test_labels)
+
+
+

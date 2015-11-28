ea474794631a1f85350766159014e85eb9fbf3b6
Michał Libura <liburam@gmail.com>
2015-11-28 11:25:02 +0100

    DirSource

--- getText.R
+++ getText.R
@@ -1,19 +1,4 @@
-library(magrittr)
-list.files('C:/Users/Lenovo/Desktop/prepared_data2/docs_collection_baseform2') %>%
-        grep('txt', x = ., value = T) %>%
-        file.path('C:/Users/Lenovo/Desktop/prepared_data2/docs_collection_baseform2' ,.) %>% 
-        sapply(function(element){
-                readLines(element) %>% 
-                        assign( x = tail(strsplit(element, "/")[[1]],1), 
-                                value = .,
-                                envir = .GlobalEnv)
-        }) %>% invisible()
+Dir = DirSource(directory = '', encoding = "UTF-8")
+corpus = VCorpus(Dir)
+DTM = DocumentTermMatrix(corpus)
 
-ls() -> files
-
-listP <- list()
-for(i in files){
-        listP[[i]] <- get(i, envir = .GlobalEnv)
-}
-
-do.call(rbind, listP) -> pliki_df

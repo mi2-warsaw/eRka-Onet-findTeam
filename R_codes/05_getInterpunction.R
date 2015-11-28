73895c5417597f28d3d9735d9a153474c4297151
Michal Stechly <michal.stechly@gmail.com>
2015-11-28 13:35:37 +0100

    Poprawione pliki

--- R_codes/05_getInterpunction.R
+++ R_codes/05_getInterpunction.R
@@ -16,32 +16,30 @@ punctuation_vector <- text_vector[text_punctuation_positions]
 punctuation_table <- as.data.frame(table(punctuation_vector))
 names(punctuation_table) <- c("sign", "freq")
 end_sign_table <- subset(punctuation_table,punctuation_table$sign %in% c(".", "!", "?","..."))
-
-
 return (end_sign_table)
 }
 
-countCoefficient <- function(end_sign_table){
+countratioicient <- function(end_sign_table){
 number_of_end_signs <- sum(end_sign_table$freq)
-exlamation_sign_table <- subset(end_sign_table,end_sign_table$sign!=".")
+tabloid_sign_table <- subset(end_sign_table,end_sign_table$sign!=".")
 
-if(nrow(exlamation_sign_table) !=0 ) {
-  number_of_exclamations <- sum(exlamation_sign_table$freq)
+if(nrow(tabloid_sign_table) !=0 ) {
+  number_of_tabloid_signs <- sum(tabloid_sign_table$freq)
 }else{
-  number_of_exclamations <- 0
+  number_of_tabloid_signs <- 0
 }
-return(number_of_exclamations/number_of_end_signs)
+return(number_of_tabloid_signs/number_of_end_signs)
 }
 
-getExclamationCoefficient <- function(filename){
+getTabloidSignRatio <- function(filename){
   endSignTable <- getEndSignTable(filename)
-  coeff <- countCoefficient(endSignTable)
-  return(coeff)
+  ratio <- countratioicient(endSignTable)
+  return(ratio)
 }
 
 rawFiles <- list.files(pattern="18*.txt",full.names=T,recursive=F)
 
 for (i in 1:5) {
-  coeff <- getExclamationCoefficient(rawFiles[i])
-  print(coeff)
+  ratio <- getTabloidSignRatio(rawFiles[i])
+  print(ratio)
 }

26d2d7b038a103ed08d2063f879e55ad80150969
MarcinKosinski <kosinskim@student.mini.pw.edu.pl>
2015-11-28 11:36:59 +0100

    add toDF.R

---
+++ 03_toDF_toMatrix.R
@@ -0,0 +1,12 @@
+library(tm)
+removeSparseTerms(DTM, sparse = 0.95) -> DTM95
+
+library(Matrix)
+
+library("Matrix") 
+mat <- sparseMatrix(i=DTM95$i, j=DTM95$j, x=DTM95$v,
+												dims=c(DTM95$nrow, DTM95$ncol))
+dim(mat)
+colnames(mat) <- DTM95$dimnames$Terms
+
+as.data.frame(as.matrix(mat)) -> DTM_df

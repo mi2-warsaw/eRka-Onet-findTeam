2cb8e4c75134a8ae6507a759384e5a885c6aed9e
Michal Stechly <michal.stechly@gmail.com>
2015-11-28 14:45:24 +0100

    Trzeci sukces. Trzecie Coś.

--- R_codes/06_addTags.R
+++ R_codes/06_addTags.R
@@ -8,3 +8,8 @@ colnames(tags) = c("uuid_h2", "Tags")
 tags$uuid_h2 = as.factor(tags$uuid_h2)
 DTM_tagged = inner_join(DTM_df, tags, by = "uuid_h2")
 
+Tags_table <- table(DTM_tagged$uuid_h2, as.character(DTM_tagged$Tags))
+
+Classes<-apply(as.matrix(Tags_table),MARGIN = 1, function(x){ifelse(x[1]>x[2],0,ifelse(x[1]==x[2],NA,1))})
+
+table(Classes,useNA = "always")
